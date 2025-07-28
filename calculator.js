952cc1f81fcf79e6090d67d6f6cfb0c664dfc6db
Kiki Chen <142045045+calciferchang@users.noreply.github.com>
2025-07-28 17:29:03 -0400

    built basic ui function skeleton

---
+++ calculator.js
@@ -0,0 +1,21 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+let firstNum = 0;
+let secondNum = 2;
+let operator;
+
+function operate(firstNum, secondNum, operator) {}
