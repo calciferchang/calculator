ef3e8cd423321c69e5bfe61f3b1a5fb18db84a45
Calcifer Chang <142045045+calciferchang@users.noreply.github.com>
2025-08-27 18:11:04 -0400

    create function for handling inputs

--- calculator.js
+++ calculator.js
@@ -1,27 +1,56 @@
 // Handle inputs
 document.addEventListener("keydown", (event) => {
-  const value = event.key;
-  const type = inputType(event);
-  if (type) {
-    handleInput(type, value);
-  }
+  handleInput(event.key);
 });
 
 const buttons = document.querySelectorAll(".button");
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     const value = button.innerText;
-    const type = inputType(value);
-    handleInput(type, value);
+    handleInput(value);
   });
 });
 
+function handleInput(input) {
+  switch (input) {
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+      return addDigitTo(currentInput);
+    case "*":
+    case "/":
+    case "-":
+    case "+":
+      return addOperatorTo(currentInput);
+    case ".":
+      return addDecimalTo(currentInput);
+    case "Enter":
+    case "=":
+      return runCalculator();
+    case "Escape":
+    case "clear ":
+      return clearCalculator();
+    case "Backspace":
+      return backspaceInput(currentInput);
+    default:
+      return console.log("walrus sucks");
+  }
+}
+
+// calculator constants
 const NUM1 = {
   value: "",
   hasDecimal: false,
 };
 const NUM2 = {
-  value: "1",
+  value: "",
   hasDecimal: false,
 };
 
