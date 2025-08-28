83125b7d625dfcd2977b9b05141af3a6c9bceb06
Calcifer Chang <142045045+calciferchang@users.noreply.github.com>
2025-08-28 00:51:57 -0400

    actually commit the prev. commit

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-// Handle inputs
+// Recognize inputs & what to do with them
 document.addEventListener("keydown", (event) => {
   handleInput(event.key);
 });
@@ -13,6 +13,7 @@ buttons.forEach((button) => {
 
 function handleInput(input) {
   if (["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].includes(input)) {
+    console.log(input);
     return addDigit(input);
   }
   if (["-", "/", "*", "+"].includes(input)) {
@@ -29,9 +30,7 @@ function handleInput(input) {
   }
 }
 
-function clearInputs() {}
-function addDigit() {}
-// calculator constants
+// Declare the calculator constants
 const NUM1 = {
   value: "",
   hasDecimal: false,
@@ -40,6 +39,47 @@ const NUM2 = {
   value: "",
   hasDecimal: false,
 };
+let operator = "";
 
+//calculator functions
+function addDigit(input) {
+  if (input === "0" && !currentInput.value) {
+    console.log("no 0 at start");
+    return;
+  } else currentInput.value = currentInput.value + input;
+}
+
+function addDecimal() {
+  //if hasDecimal true, do not run
+  if (currentInput.hasDecimal) {
+    return;
+  }
+  if (!currentInput.value) {
+    currentInput.hasDecimal = true;
+    return (currentInput.value = "0.");
+  }
+  currentInput.value = currentInput.value + ".";
+  currentInput.hasDecimal = true;
+}
+function useOperator() {
+  if (NUM2.value) {
+    runCalculator();
+  }
+  //set operator
+  //set currentinput to num2
+}
+function clearInputs() {
+  //reset NUM1
+  //reset NUM2
+  //reset operator
+  //set currentinput to NUM1
+}
+
+function runCalculator() {
+  //look at operator
+  //choose which function to put numbers in based off operator
+  //put result into NUM1, reset operator and NUM2
+  //set current input to NUM1
+}
 // First run
 let currentInput = NUM1;
